45ad591058cc5474bdfcccc91b4c413ec1328faa
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-08-21 11:08:27 -0400

    react app initialized

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <h1>Portfolio</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
