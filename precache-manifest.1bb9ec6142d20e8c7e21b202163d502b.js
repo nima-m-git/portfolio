05984ec9f94bb915cbaf73bcee9a7567cd2525f0
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-08-24 17:55:39 -0400

    Updates

---
+++ precache-manifest.1bb9ec6142d20e8c7e21b202163d502b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f380bc8d54864e9f3dcaa1bf0a7f78",
+    "url": "/nima-m-git/portfolio/index.html"
+  },
+  {
+    "revision": "484498d0ef8ee733ffc3",
+    "url": "/nima-m-git/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3a0c7623a845c9c57d7d",
+    "url": "/nima-m-git/portfolio/static/css/main.880897bb.chunk.css"
+  },
+  {
+    "revision": "484498d0ef8ee733ffc3",
+    "url": "/nima-m-git/portfolio/static/js/2.a78aec96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nima-m-git/portfolio/static/js/2.a78aec96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a0c7623a845c9c57d7d",
+    "url": "/nima-m-git/portfolio/static/js/main.dd1615de.chunk.js"
+  },
+  {
+    "revision": "d581b3a5f559e2db1ab9",
+    "url": "/nima-m-git/portfolio/static/js/runtime-main.7ee020b3.js"
+  }
+]);
