bba0f4f8892b0279c5ae4058ef3250d668de8518
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-08-31 12:12:03 -0400

    about contect filled

--- src/components/about.js
+++ src/components/about.js
@@ -8,12 +8,30 @@ const About = () => {
                 <p><big>Hello,</big></p>
                 <p>
                     <br></br>
-                    I'm Nima, a fullstack software developer from Toronto.
+                    I'm Nima, a software developer from Toronto.
                     <br></br>This is a bit of my background history, interests, and education.
-                    And this is why i chose software development 
+                    And this is why i chose software development.
+
+                    I received my Bachelor of Science in Chemistry in 2017. Why chemistry? I'm a very curious person, I like to know how things work - chemistry is the base and building block of everything.
+                    How are you seeing this right now? In very short; Many photons (light particles) being emitted at certain wavelengths (giving their color), determined by your computer through the flow of electromagnetic fields to various materials composing the circuitry (translating to digital information).
+                    How do your eyes receive these images? How does your brain translate optical information to meaning? How do you remember these things? Why are you motivated to do any of this? How does your body work? Why is the sky blue?
+                    As an ambitious teenager, I believed I could use this knowledge professionally to help improve the world somehow: develope life-saving medicine, bio-hack the genome to induce regeneration, discover a favourable process to break down plastics.   
+                    ..You get the point, I could go on forever - and I love to actually, feel free to <a href='/contact'>contact me</a> to chat all things science.
+
+
+                    Why programming? 
+                    I realized working as a lab-rat wasn't as fulfilling as I had once thought, nor did most directions I could take from there seem, I knew I needed to do something else. 
+                    I spent some quality time thouroughly analyzing what I enjoy doing, what I'm good at, and what realistic career prospects those could tie in to. 
+                    I deduced that I enjoy being creative, am a logical and 'outside the box' thinker, highly efficient and get great satisfaction out of improving efficiency of all things, highly analytical, enjoy problem solving (most fun part of school), and need a sense of reward with the work I do.
+                    The top 3 matches I found <i>at that time</i> were Entrapaneurship, Business Strategy Consulting, and Project Management. Unfortunately none of these were realistically attainable from the position I was in. <small>I still plan on integrating some of these with Tech, <a href='/contact'>ask me more</a> if you're curious.</small>
+                    At the same time I was working in two places, a doctors office and a digital media agency. I wanted to contribute more than the basic tasks I was doing, which otheres were already doing, in more unique and lasting ways. I set up a voice transcribing software with simplified accompaying instructions for the doctor, started writing macros in excel/VBA for repetitive manual administrative tasks at the digital media agency - reached the limitations with VBA, then started learning Python to expand what I could build.
+                    Thus my programming journey began.
+                    
+                    I enjoy building all kinds of software, and am also a statistics and data person, with great interest in data science/machine learning and their applications. 
                 </p>
                 <p>
-                    Need something built? Want to collbaroate on a project? Just chat? <a href=''>Send me a message</a>
+                    Need something built? Improved? Meaningful insights from data? Collbaroate on a project? Invest in an idea that could change the world? 
+                    Or just chat about anything? <a href=''>Send me a message</a>
                 </p>
             </section>
         </div>
