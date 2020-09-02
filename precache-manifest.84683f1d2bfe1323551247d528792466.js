f6843d0e35c10df2cdd8b2904e3823ba24ede284
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-09-02 00:53:54 -0400

    Updates

---
+++ precache-manifest.84683f1d2bfe1323551247d528792466.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e8d2620b353a78c096b841a1dc531d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "34310e1bb5d00101fb11",
+    "url": "/portfolio/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e9f3e219ea2e64507e68",
+    "url": "/portfolio/static/css/main.d5d1dfa0.chunk.css"
+  },
+  {
+    "revision": "34310e1bb5d00101fb11",
+    "url": "/portfolio/static/js/2.c0945bdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c0945bdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f3e219ea2e64507e68",
+    "url": "/portfolio/static/js/main.ee9f6eec.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4c379f95226468ec3b950390e8a12b4a",
+    "url": "/portfolio/static/media/NEON GLOW.4c379f95.otf"
+  },
+  {
+    "revision": "e9c75680f5de08c7cbe010a8eff86836",
+    "url": "/portfolio/static/media/icons8-cloud-firestore.e9c75680.svg"
+  },
+  {
+    "revision": "1f9f8fdc49a961344a8db2917bd32cac",
+    "url": "/portfolio/static/media/icons8-css3.1f9f8fdc.svg"
+  },
+  {
+    "revision": "4e173420c6b0f326d457597902a35d46",
+    "url": "/portfolio/static/media/icons8-django.4e173420.svg"
+  },
+  {
+    "revision": "a33afa2f2b5c056049002600e4c8944c",
+    "url": "/portfolio/static/media/icons8-firebase.a33afa2f.svg"
+  },
+  {
+    "revision": "0a27096e6f64068e40936f1763b906c0",
+    "url": "/portfolio/static/media/icons8-git.0a27096e.svg"
+  },
+  {
+    "revision": "a522aecde9b25d951fbe679803886d6d",
+    "url": "/portfolio/static/media/icons8-html-5.a522aecd.svg"
+  },
+  {
+    "revision": "f607a6b7bdf5aeefc5b2483ad117efc8",
+    "url": "/portfolio/static/media/icons8-javascript.f607a6b7.svg"
+  },
+  {
+    "revision": "d3deb2f676c2bbbdf915e318864bd63f",
+    "url": "/portfolio/static/media/icons8-nodejs.d3deb2f6.svg"
+  },
+  {
+    "revision": "776dbdbde51183ac8e1472ec493e4a90",
+    "url": "/portfolio/static/media/icons8-postgresql.776dbdbd.svg"
+  },
+  {
+    "revision": "1a4e97681490d58b5fe6077e3a95698c",
+    "url": "/portfolio/static/media/icons8-python.1a4e9768.svg"
+  },
+  {
+    "revision": "a75f9e03b586891d8e44fc6e5d94bb4f",
+    "url": "/portfolio/static/media/icons8-react.a75f9e03.svg"
+  },
+  {
+    "revision": "80ddcd5bd4847b07f586f0c502f6d490",
+    "url": "/portfolio/static/media/icons8-sass.80ddcd5b.svg"
+  },
+  {
+    "revision": "b8878827cda95e5c014f367aa90f78ff",
+    "url": "/portfolio/static/media/icons8-webpack.b8878827.svg"
+  },
+  {
+    "revision": "963b162ac19d37332998d77d6675c469",
+    "url": "/portfolio/static/media/mysql-icon.963b162a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.94a40dc6c687e9477bdc8af0d1fcad08.js"
+  "/portfolio/precache-manifest.84683f1d2bfe1323551247d528792466.js"
 );
 
 self.addEventListener('message', (event) => {
