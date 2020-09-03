d4876f5f0b47f285a07cf73791c0cac442fad6e7
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-09-03 13:40:07 -0400

    about content updated

--- src/components/about.js
+++ src/components/about.js
@@ -4,38 +4,39 @@ import React from 'react';
 const About = () => {
     return (
         <div className='about container'>
+            <h1>About</h1>
             <section className='info'>
                 <div>
                     <p>
-                        <big>Background..</big>
-                    </p>
-                    <p>
-                        I received my Bachelor of Science in Chemistry in 2017. Why chemistry? I'm a very curious person, I like to know how things work - chemistry is the base and building block of everything.
-                        <br></br>How are you seeing this right now? In very short; Many photons (light particles) being emitted at certain wavelengths (giving their color), determined by your computer through the flow of electromagnetic fields to various materials composing the circuitry (translating to digital information).
-                        How do your eyes receive these images? How does your brain translate optical information to meaning? How do you remember these things? Why are you motivated to do any of this? How does your body work? Why is the sky blue?
-                        As an ambitious teenager, I believed I could use this knowledge professionally to help improve the world somehow: develope life-saving medicine, bio-hack the genome to induce regeneration, discover a favourable process to break down plastics.   
-                        ..You get the point, I could go on forever - and I love to actually, feel free to <a href='/contact'>contact me</a> to chat all things science.
-                    </p>
-                    <p>
-                        Why programming? 
-                        I realized working as a lab-rat wasn't as fulfilling as I had once thought, nor did most directions I could take from there seem, I knew I needed to do something else. 
-                        I spent some quality time thouroughly analyzing what I enjoy doing, what I'm good at, and what realistic career prospects those could tie in to. 
-                        I deduced that I enjoy being creative, am a logical and 'outside the box' thinker, highly efficient and get great satisfaction out of improving efficiency of all things, highly analytical, enjoy problem solving (most fun part of school), and need a sense of reward with the work I do.
-                        The top 3 matches I found <i>at that time</i> were Entrapaneurship, Business Strategy Consulting, and Project Management. Unfortunately none of these were realistically attainable from the position I was in. <small>I still plan on integrating some of these with Tech, <a href='/contact'>ask me more</a> if you're curious.</small>
-                        At the same time I was working in two places, a doctors office and a digital media agency. I wanted to contribute more than the basic tasks I was doing, which otheres were already doing, in more unique and lasting ways. I set up a voice transcribing software with simplified accompaying instructions for the doctor, started writing macros in excel/VBA for repetitive manual administrative tasks at the digital media agency - reached the limitations with VBA, then started learning Python to expand what I could build.
+                        <span class='big'>Why programming?</span><br></br>
+                        I spent some quality time thouroughly analyzing what I enjoy doing, what I'm good at, and what realistic prospects those could tie in to. 
+                        I deduced that I enjoy being creative, am a logical and 'outside the box' thinker, highly efficient and get great satisfaction out of improving efficiencies, highly analytical, enjoy problem solving, and rewarding work.
+                        Programming came in the top of these, along with entrapaneurship and business strategy consulting. I'd like to develop digital solutions to automate and improve business efficiencies.
+                        Around winter 2018 I was working at a doctors office and a digital media agency. I wanted to contribute more than the basic tasks I was doing, in unique and lasting ways. I set up a voice transcribing software for the doctor, and started writing macros in Excel/VBA to automate repetitive manual administrative tasks at the digital agency - reached the limitations of that technology and started learning Python to expand my building capabilities.
                         Thus my programming journey began.
                     </p>
                     <p>
-                        I enjoy building all kinds of software, and am also a statistics and data person, with great interest in data science/machine learning and their applications.
+                        I enjoy building all kinds of software, and have great interest in data science & machine learning applications.
                     </p>
                     <p>
+                        <span class='big'>Education</span><br></br>
+                        I received my Bachelor of Science in Chemistry in 2017. Why chemistry? I'm a very curious person, I like to know how things work - chemistry is the base and building block of everything.
+                        <br></br>E.g- How are you seeing this right now? Short; Many photons (light particles) emitted at specific wavelengths (giving their color), determined by your computer through the flow of electromagnetic fields to various materials composing the circuitry (translating to digital information).
+                        How do your eyes receive these images? How does your brain translate optical information to meaning? How do you process information? Why do you want to do any of this? Why is the sky blue?
+                        I wanted to use this knowledge could be used to help the world somehow: develop life-saving medicine, bio-hack the genome to induce regeneration, discover a favourable process to break down plastics.   
+                        ..You get the point, I could go on forever - and I love to actually, feel free to <a href='/contact'>contact me</a> to chat all things science.
+                    </p>
+
+                    <p>
+                        <span class='big'>Hobbies</span><br></br>
                         I have a wide array of interests: science, technology, business, economics, politics, history, music, art, (some) sports, food, culture, you name it. <a href='/contact'>Talk to me</a> about anything. 
                         I like learning new things, travelling the world, exploring the outdoors, music, cooking, eating, sunsets, sports, weightlifting, board/card games, other organisms.
                     </p>
                     <p>
-                        Need something built? Improved? Meaningful insights from data? Collbaroate on a project? Invest in an idea that could change the world? 
+                        <span class='big'>Contact</span><br></br>
+                        Need something built? Improved? Meaningful insights from data? Collaborate on a project? Invest in an idea that could change the world? 
                         New friend? Chat about anything? 
-                        <a href='/contact'>Send me a message</a>
+                        <br></br><a href='/contact'>Send me a message</a>
                     </p>
                 </div>
 
