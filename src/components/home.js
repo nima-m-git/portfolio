c1e087a5db4eb0d1ecbd38c612de9ae647c91230
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-09-03 11:31:56 -0400

    text transition homepage added

--- src/components/home.js
+++ src/components/home.js
@@ -6,15 +6,17 @@ const Home = () => {
         <div className='home container'>
             <section className='info'>
                 <div>
-                    <p>
+                    <p className='load-1'>
                         <big>Hello,</big>
                     </p>
-                    <p>
+                    <p className='load-2'>
                         I'm Nima, a software developer from Toronto.
                     </p>
-                    <p>
+                    <p className='load-3'>
                         Need something built? Improved? Meaningful insights from data? Collbaroate on a project? Invest in an idea that could change the world? 
                         New friend? Chat about anything? 
+                    </p>
+                    <p className='load-4'>
                         <a href='/contact'> Send me a message</a>
                     </p>
                 </div>
