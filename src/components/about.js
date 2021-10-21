ad98597ec80238a9ac2a3bf5a688466d60f95ec6
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2021-10-21 18:20:54 -0400

    refactored Technologies.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,43 +1,62 @@
-import React from 'react';
-
+import React from "react";
 
 const About = () => {
-    return (
-        <div className='about container'>
-            <section className='info'>
-                <div>
-                    <p>
-                        <span className='big'>Why programming?</span><br></br>
-                        I spent some quality time post university thouroughly analyzing what I enjoy doing, what I'm good at, and what prospects would suit. 
-                        <br></br>I deduced that I enjoy creating, am highly analytical, a logical and 'outside the box' thinker, enjoy solving problems, and feel great reward from making things more efficient.
-                        Programming came in the top of these, along with entrapaneurship and business strategy consulting. I'd like to develop digital solutions to improve all kinds of business efficiencies.
-                        <br></br>In winter of 2018, I wanted to contribute more than the basic tasks I was doing at my then work places in unique and lasting ways, I began learning and implementing automation tools for repetitive manual tasks -
-                        Thus my programming journey began.
-                    </p>
-                    <p>
-                        I enjoy building all kinds of software, solvings various problems, and have great interest in artificial intelligence and everything data.
-                        I have yet to come across a software problem I've refused to figure out.
-                    </p>
-                    <p>
-                        <span className='big'>Education</span><br></br>
-                        I received my Bachelors of Science in Chemistry in 2017. Why chemistry? I'm a very curious person, I like to know how things work - chemistry is the base and building block of everything we know.
-                        {/* <br></br>E.g- How are you seeing this right now? Short; Many photons (light particles) emitted at specific wavelengths (giving their color), determined by your computer through the flow of electromagnetic fields to various materials composing the circuitry (translating to digital information).
+  return (
+    <div className="about container">
+      <section className="info">
+        <div>
+          <p>
+            <span className="big">Why programming?</span>
+            <br></br>I spent some quality time post university thouroughly
+            analyzing what I enjoy doing, what I'm good at, and what prospects
+            would suit.
+            <br></br>I deduced that I enjoy creating, problem solving, am highly
+            analytical, a logical and 'outside the box' thinker, and get a great
+            sense of reward from making things more efficient. Programming came
+            in the top of these, along with entrapaneurship and business
+            strategy consulting. I like to develop digital solutions to improve
+            various business problems.
+            <br></br>In winter of 2018, I wanted to contribute more than the
+            basic tasks I was doing at my then work places in unique and lasting
+            ways, I began learning and implementing automation tools for
+            repetitive manual tasks - Thus my programming journey began.
+          </p>
+          <p>
+            I enjoy building all kinds of software, solvings various problems,
+            and have great interest in artificial intelligence and everything
+            data. There's no tech problem I've refused to figure out.
+          </p>
+          <p>
+            <span className="big">Education</span>
+            <br></br>I received my Bachelors of Science in Chemistry -
+            Pharmaceutical & Biological in 2017. Why chemistry? I'm a very
+            curious person, I like to know how things work - chemistry is the
+            base and building block of everything we know.
+            {/* <br></br>E.g- How are you seeing this right now? Short; Many photons (light particles) emitted at specific wavelengths (giving their color), determined by your computer through the flow of electromagnetic fields to various materials composing the circuitry (translating to digital information).
                         How do your eyes receive these images? How does your brain translate optical information to meaning? How do you process information? Why do you want to do any of this? Why is the sky blue? */}
-                        I wanted to use this knowledge to make the world better somehow: improve the process of detecting disease bio-markers and protein binding sites to develop life-saving medicine, bio-hack the genome to induce regeneration, biodegrade plastics, build more efficient batteries, reduce atmospheric CO2  
-                        <br></br>...You get the point, I could go on forever - and I love to actually, <a href='/contact'>come chat</a> about all things science.
-                    </p>
+            I wanted to use this knowledge to make the world better somehow:
+            improve the process of detecting disease bio-markers and protein
+            binding sites to develop life-saving medicine, bio-hack the genome
+            to induce regeneration, biodegrade plastics, build more efficient
+            batteries, reduce atmospheric CO2
+            <br></br>...You get the point, I could go on forever - and I love to
+            actually, <a href="/contact">come chat</a> about all things science.
+          </p>
 
-                    <p>
-                        <span className='big'>Hobbies</span><br></br>
-                        I have a wide array of interests: science, technology, business, economics, politics, history, music, art, sports, food, culture, you name it. Feel free to <a href='/contact'>chat with me</a> about anything. 
-                        <br></br>I like learning new things, travelling the world, exploring the outdoors, music, cooking, eating, sunsets, sports, weightlifting, board/card games, observing other organisms.
-                    </p>
-                </div>
-
-            </section>
+          <p>
+            <span className="big">Hobbies</span>
+            <br></br>I have a wide array of interests: science, technology,
+            business, economics, politics, history, music, art, sports, food,
+            culture, you name it. Feel free to{" "}
+            <a href="/contact">chat with me</a> about anything.
+            <br></br>I like learning new things, travelling the world, exploring
+            the outdoors, music, cooking, eating, sunsets, sports,
+            weightlifting, board/card games, observing other organisms.
+          </p>
         </div>
-    )
-}
-
+      </section>
+    </div>
+  );
+};
 
 export default About;
--- src/components/technologies.js
+++ src/components/technologies.js
@@ -1,124 +1,111 @@
-import React from 'react';
+import React from "react";
 
-import WebpackIcon from '../icons/icons8-webpack.svg';
-import FireStoreIcon from '../icons/icons8-cloud-firestore.svg';
-import CSS3Icon from '../icons/icons8-css3.svg';
-import DjangoIcon from '../icons/icons8-django.svg';
-import FireBaseIcon from '../icons/icons8-firebase.svg';
-import GitIcon from '../icons/icons8-git.svg';
-import HTML5Icon from '../icons/icons8-html-5.svg';
-import JavascriptIcon from '../icons/icons8-javascript.svg';
-import NodeJSIcon from '../icons/icons8-nodejs.svg';
-import PostgreSQLIcon from '../icons/icons8-postgresql.svg';
-import PythonIcon from '../icons/icons8-python.svg';
-import ReactIcon from '../icons/icons8-react.svg';
-import SassIcon from '../icons/icons8-sass.svg';
-import MongoDBIcon from '../icons/icons8-mongodb.svg';
-import ExpressJSIcon from '../icons/expressjs.svg';
+import WebpackIcon from "../icons/icons8-webpack.svg";
+import FireStoreIcon from "../icons/icons8-cloud-firestore.svg";
+import CSS3Icon from "../icons/icons8-css3.svg";
+import DjangoIcon from "../icons/icons8-django.svg";
+import FireBaseIcon from "../icons/icons8-firebase.svg";
+import GitIcon from "../icons/icons8-git.svg";
+import HTML5Icon from "../icons/icons8-html-5.svg";
+import JavascriptIcon from "../icons/icons8-javascript.svg";
+import NodeIcon from "../icons/icons8-nodejs.svg";
+import PostgreSQLIcon from "../icons/icons8-postgresql.svg";
+import PythonIcon from "../icons/icons8-python.svg";
+import ReactIcon from "../icons/icons8-react.svg";
+import SassIcon from "../icons/icons8-sass.svg";
+import MongoDBIcon from "../icons/icons8-mongodb.svg";
+import ExpressIcon from "../icons/expressjs.svg";
+import AWSIcon from "../icons/icons8-amazon-web-services.svg";
 
+const icons = {
+  WebpackIcon,
+  FireStoreIcon,
+  CSS3Icon,
+  DjangoIcon,
+  FireBaseIcon,
+  GitIcon,
+  HTML5Icon,
+  JavascriptIcon,
+  NodeIcon,
+  PostgreSQLIcon,
+  PythonIcon,
+  ReactIcon,
+  SassIcon,
+  MongoDBIcon,
+  ExpressIcon,
+  AWSIcon,
+};
 
-const Technologies = (props) => (
-    <div className='technologies'>
-        <h2>Technologies</h2>
-        <div className='techs-container'>
-            {/* Combine Techs into fxn -- DRY */}
-            <div className='programming techs'>
-                <h3>Programming</h3>
-                <ul>
-                    <li>
-                        <img src={PythonIcon} alt='Python icon'className='icon'/>
-                        <span onClick={() => props.filterTech('Python')}>Python</span>                            
-                    </li>
-                    <li>
-                        <img src={JavascriptIcon} alt='Javascript icon'className='icon'/>
-                        <span onClick={() => props.filterTech('Javascript')}>Javascript</span>                            
-                    </li>
-                </ul>
-            </div>
-            <div className='Runtime techs'>
-                <h3>Runtime Envs</h3>
-                <ul>
-                    <li>
-                        <img src={NodeJSIcon} alt='Node icon'className='icon'/>
-                        <span onClick={() => props.filterTech('Node')}>NodeJS</span>                            
-                    </li>
-                </ul>
-            </div>
-            <div className='library techs'>
-                <h3>Libraries</h3>
-                <ul>
-                    <li>
-                        <img src={ReactIcon} alt='React icon'className='icon'/>
-                        <span onClick={() => props.filterTech('React')}>React</span>                            
-                    </li>
-                </ul>
-            </div>
-            <div className='framework techs'>
-                <h3>Frameworks</h3>
-                <ul>
-                    <li>
-                        <img src={DjangoIcon} alt='Django icon'className='icon'/>
-                        <span onClick={() => props.filterTech('Django')}>Django</span>                            
-                    </li>
-                    <li>
-                        <img src={ExpressJSIcon} alt='Express icon'className='icon'/>
-                        <span onClick={() => props.filterTech('Express')}>Express</span>                            
-                    </li>
-                </ul>
-            </div>
-            <div className='design techs'>
-                <h3>Design</h3>
-                <ul>
-                    <li onClick={() => props.filterTech('HTML')}>
-                        <img src={HTML5Icon} alt='HTML icon'className='icon'/>
-                        <span>HTML5</span>                            
-                    </li>
-                    <li onClick={() => props.filterTech('CSS')}>
-                        <img src={CSS3Icon} alt='CSS icon'className='icon'/>
-                        <span>CSS3</span>                            
-                    </li>
-                    <li onClick={() => props.filterTech('Sass')}>
-                        <img src={SassIcon} alt='Sass icon'className='icon'/>
-                        <span>Sass</span>                            
-                    </li>
-                </ul>
-            </div>
-            <div className='databases techs'>
-                <h3>Databases</h3>
-                <ul>
-                    <li onClick={() => props.filterTech('PostgreSQL')}>
-                        <img src={PostgreSQLIcon} alt='PostgreSQL icon'className='icon'/>
-                        <span>PostgreSQL</span>                            
-                    </li>
-                    <li onClick={() => props.filterTech('MongoDB')}>
-                        <img src={MongoDBIcon} alt='MongoDB icon'className='icon'/>
-                        <span>MongoDB</span>                            
-                    </li>
-                    <li onClick={() => props.filterTech('Firestore')}>
-                        <img src={FireStoreIcon} alt='FireStore icon'className='icon'/>
-                        <span>FireStore</span>                            
-                    </li>
-                </ul>
-            </div>
-            <div className='other techs'>
-                <h3>Tools</h3>
-                <ul>
-                    <li>
-                        <img src={WebpackIcon} alt='webpack icon' className='icon'/>
-                        <span>Webpack</span>                            
-                    </li>
-                    <li>
-                        <img src={FireBaseIcon} alt='Firebase icon'className='icon'/>
-                        <span>Firebase</span>                            
-                    </li>
-                    <li>
-                        <img src={GitIcon} alt='Git icon'className='icon'/>
-                        <span>Git</span>                            
-                    </li>
-                </ul>
-            </div>
-        </div>
+const TECHS = [
+  {
+    type: "Programming",
+    kinds: ["Python", "Javascript"],
+  },
+  {
+    type: "Runtime Envs",
+    kinds: ["Node"],
+  },
+  {
+    type: "Libraries",
+    kinds: ["React", "Framer-Motion"],
+  },
+  {
+    type: "Frameworks",
+    kinds: ["Django", "Express"],
+  },
+  {
+    type: "Databases",
+    kinds: ["MongoDB", "PostgreSQL", "FireStore"],
+  },
+  {
+    type: "Design",
+    kinds: ["Sass", "CSS3", "HTML5"],
+  },
+  {
+    type: "Other",
+    kinds: ["Webpack", "FireBase"],
+  },
+  {
+    type: "Cloud",
+    kinds: ["AWS"],
+  },
+];
+
+const TYPES_TO_FILTER = [
+  "Programming",
+  "Runtime Env",
+  "Libraries",
+  "Frameworks",
+  "Databases",
+];
+
+const Technologies = ({ filterTech }) => (
+  <div className="technologies">
+    <h2>Technologies</h2>
+    <div className="techs-container">
+      {TECHS.map(({ type, kinds }) => {
+        const filterKind = TYPES_TO_FILTER.includes(type);
+
+        return (
+          <div className={type + " techs"} key={type}>
+            <h3>{type}</h3>
+            <ul>
+              {kinds.map((kind) => (
+                <li key={kind}>
+                  {icons[kind + "Icon"] && (
+                    <img src={icons[kind + "Icon"]} alt="" className="icon" />
+                  )}
+                  <span onClick={() => filterKind && filterTech(kind)}>
+                    {kind}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
-)
+  </div>
+);
 
 export { Technologies };
